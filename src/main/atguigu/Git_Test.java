f41f86c53c78882b011f7257f8bcaba4f2602983
yun <1737825818@qq.com>
2022-08-14 11:12:54 +0800

    Initial commit

---
+++ src/main/atguigu/Git_Test.java
@@ -0,0 +1,23 @@
+package main.atguigu;
+
+/**
+ * @author 帅炸的云大仔仔
+ * @date 2022/8/14 - 11:08
+ */
+public class Git_Test {
+    /**
+     * @author 帅炸的云大仔仔
+     * @date 2022/7/14 - 14:32
+     */
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+    }
+}
+
